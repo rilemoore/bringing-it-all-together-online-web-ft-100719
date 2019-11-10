085e9393bf14b7dc826898f3c68d539deec54429
Riley Moore <rilemoore@gmail.com>
2019-11-10 21:01:38 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,57 @@
+class Dog
+  attr_accessor :name, :breed
+  attr_reader :id
+  
+  def initialize(name:, breed:, id: nil)
+    # @name = info_hash[:name]
+    # @breed = info_hash[:breed]
+    # @id = info_hash[:id]
+    @name = name
+    @breed = breed
+    @id = id
+  end
+  
+  def self.create_table
+    sql = "CREATE TABLE IF NOT EXISTS dogs(id INTEGER PRIMARY KEY, name TEXT, breed TEXT);"
+    DB[:conn].execute(sql)
+  end
+  
+  def self.drop_table
+    sql = "DROP TABLE IF EXISTS dogs"
+    DB[:conn].execute(sql)
+  end
+  
+  def self.create(name, breed)
+    dog = self.new(name, breed)
+    dog.save
+    dog
+  end
+  
+  def self.new_from_db(info)
+    new_dog = self.new(info[1], info[2], info[0])
+    new_dog
+  end
+  
+  def self.find_by_name(name)
+    sql = "SELECT * FROM dogs WHERE name = ?"
+    info = DB[:conn].execute(sql, name).flatten
+    self.new_from_db(info)
+  end
+  
+  def save
+    if self.id
+      self.update
+    else
+      sql = "INSERT INTO dogs (name, breed) VALUES (?, ?)"
+      DB[:conn].execute(sql, self.name, self.breed)
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+      binding.pry
+    end
+  end
+  
+  def update
+    sql = "UPDATE dogs SET name = ?, breed = ? WHERE id = ?"
+    DB[:conn].execute(sql, @name, @breed, @id)
+  end
+  
+end
