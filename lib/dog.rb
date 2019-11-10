963ac5c26c204ad49c3fd14e98db149a19c46095
Riley Moore <rilemoore@gmail.com>
2019-11-10 21:08:40 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -46,6 +46,7 @@ class Dog
       DB[:conn].execute(sql, self.name, self.breed)
       @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
       DB[:conn].execute("SELECT * FROM dogs ORDER BY id DESC LIMIT 1").flatten
+      self
     end
   end
   
